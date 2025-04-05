e0eb8a20834503541b5262a24d4def2817dc128d
Youssef <youssefabdelrazk2002@gmail.com>
2025-04-05 14:44:44 +0200

    implement job listing and search functionality with React Query integration

--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -1,3 +1,22 @@
-export default function JopsPage() {
-  return <h1 className=' text-4xl font-bold text-center mt-10'>jobs page</h1>;
+import JobsList from '@/components/JobsList';
+import SearchForm from '@/components/SearchForm';
+import { getAllJobsAction } from '@/utils/actions';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+
+export default async function JopsPage() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['jobs', '', 'all', 1],
+    queryFn: () => getAllJobsAction({}),
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <SearchForm />
+      <JobsList />
+    </HydrationBoundary>
+  );
 }
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,11 +1,10 @@
 'use server';
 
-import prisma from './db';
 import { auth } from '@clerk/nextjs';
-import { JobType, CreateAndEditJobType, createAndEditJobSchema } from './types';
-import { redirect } from 'next/navigation';
 import { Prisma } from '@prisma/client';
-import dayjs from 'dayjs';
+import { redirect } from 'next/navigation';
+import prisma from './db';
+import { CreateAndEditJobType, JobType, createAndEditJobSchema } from './types';
 
 function authUserId(): string {
   const { userId } = auth();
@@ -30,6 +29,83 @@ export async function CreateJobAction(
     return job;
   } catch (error) {
     console.log(error, 'error');
+    return null;
+  }
+}
+
+type GetAllJobsActionTypes = {
+  search?: string;
+  jobStatus?: string;
+  page?: number;
+  limit?: number;
+};
+
+export async function getAllJobsAction({
+  search,
+  jobStatus,
+  page = 1,
+  limit = 10,
+}: GetAllJobsActionTypes): Promise<{
+  jobs: JobType[];
+  count: number;
+  page: number;
+  totalPages: number;
+}> {
+  const userId = authUserId();
+
+  try {
+    let whereClause: Prisma.JobWhereInput = {
+      clerkId: userId,
+    };
+    if (search) {
+      whereClause = {
+        ...whereClause,
+        OR: [
+          {
+            position: {
+              contains: search,
+            },
+          },
+          {
+            company: {
+              contains: search,
+            },
+          },
+        ],
+      };
+    }
+    if (jobStatus && jobStatus !== 'all') {
+      whereClause = {
+        ...whereClause,
+        status: jobStatus,
+      };
+    }
+
+    const jobs: JobType[] = await prisma.job.findMany({
+      where: whereClause,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return { jobs, count: 0, page: 1, totalPages: 0 };
+  } catch (error) {
+    console.error(error);
+    return { jobs: [], count: 0, page: 1, totalPages: 0 };
+  }
+}
+export async function deleteJobAction(id: string): Promise<JobType | null> {
+  const userId = authUserId();
+
+  try {
+    const job: JobType = await prisma.job.delete({
+      where: {
+        id,
+        clerkId: userId,
+      },
+    });
+    return job;
+  } catch (error) {
     return null;
   }
 }
